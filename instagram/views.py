65040f75e17d0fb3a9cc0318e6b959d73207e55d
Clemo97 <lumumbaclement@gmail.com>
2022-06-06 23:34:20 +0300

    login require view function

--- instagram/views.py
+++ instagram/views.py
@@ -10,3 +10,16 @@ from django.contrib.auth.models import User
 def index(request):
     title='Home'
     return render(request,"index.html",{"title":title})
+
+@login_required(login_url="/accounts/login/")
+def stories(request):
+    try:
+        current_user=request.user.id
+        images=Image.objects.all()
+        profile_image=Profile.objects.filter(userId=current_user)
+        profile=profile_image.reverse()[0:1]
+        users=User.objects.all().exclude(id=request.user.id)
+        comments=Comments.objects.all()
+        #comments
+    except Exception as e:
+        raise Http404()
