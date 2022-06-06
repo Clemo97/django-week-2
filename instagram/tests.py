26f9c331cf71efe92ee5721f910f52c4a61bd30e
Clemo97 <lumumbaclement@gmail.com>
2022-06-06 22:04:55 +0300

    test profile class

--- instagram/tests.py
+++ instagram/tests.py
@@ -3,3 +3,13 @@ from django.contrib.auth.models import User
 from .models import Profile,Comments,Image,Followers
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.collo=Profile(pic="img.png",bio="sexy",userId=1)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.collo,Profile))
+
+    def test_initialization(self):
+        self.assertEqual(self.collo.pic,"img.png")
+        self.assertEqual(self.collo.bio,"sexy")
+        self.assertEqual(self.collo.userId,1)
